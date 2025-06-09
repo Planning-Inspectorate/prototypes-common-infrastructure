3c25f34a8902ce20fc4fd6bfb909539ef9710094
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-06-09 10:32:49 +0100

    feat: use docker for the application stack

--- infrastructure/app-web.tf
+++ infrastructure/app-web.tf
@@ -15,11 +15,9 @@ resource "azurerm_linux_web_app" "web_app" {
 
 
   app_settings = {
-    NODE_ENV                            = "production"
-    PASSWORD                            = each.value["password"]
-    WEBSITE_RUN_FROM_PACKAGE            = 0
-    PORT                                = 8080
-    WEBSITES_CONTAINER_START_TIME_LIMIT = 600
+    NODE_ENV = "production"
+    PASSWORD = each.value["password"]
+    PORT     = 8080
   }
 
   resource_group_name = azurerm_resource_group.primary.name
@@ -38,12 +36,13 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 
   site_config {
-    always_on        = true
-    http2_enabled    = true
-    app_command_line = "npm ci && npm run start"
+    always_on                               = true
+    http2_enabled                           = true
+    container_registry_use_managed_identity = true
 
     application_stack {
-      node_version = "20-lts"
+      docker_image_name   = "${each.value["image_name"]}:main"
+      docker_registry_url = "https://${data.azurerm_container_registry.acr.login_server}"
     }
 
     ip_restriction_default_action = "Allow"
@@ -56,6 +55,7 @@ resource "azurerm_linux_web_app" "web_app" {
       # ignore any changes to the docker image, since the image tag changes per deployment
       # all other site_config and application_stack changes should be tracked
       # see state file to check structure: site_config and application_stack are arrays in state, with a single entry
+      site_config[0].application_stack[0].docker_image_name,
       # ignore any changes to "hidden-link" and other tags
       # see https://github.com/hashicorp/terraform-provider-azurerm/issues/16569
       tags
@@ -68,5 +68,13 @@ resource "azurerm_role_assignment" "app_web_secrets_user" {
   for_each             = local.app_services
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_web_app.web_app[each.key].identity[0].principal_id
+}
+
+## RBAC for ACR pull
+resource "azurerm_role_assignment" "app_web_acr_pull" {
+  for_each             = local.app_services
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
   principal_id         = azurerm_linux_web_app.web_app[each.key].identity[0].principal_id
 }
--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -18,3 +18,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azurerm" {
+  alias           = "tooling"
+  subscription_id = "edb1ff78-90da-4901-a497-7e79f966f8e2"
+  features {}
+}
