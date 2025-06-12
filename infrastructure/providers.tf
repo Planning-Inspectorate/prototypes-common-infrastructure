5c7277281a3893186e5710b2f0cfdef719731673
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-06-12 14:43:40 +0100

    chore: update azurerm to 4.32.0

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.29.0"
+      version = "> 4"
     }
   }
   required_version = ">= 1.7.3, < 1.12.0"
