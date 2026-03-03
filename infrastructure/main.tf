b3597db4c035030f01060cfd79a1f2849c326edc
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-03 11:19:40 +0000

    feat: adding in private endpoints

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,19 +14,21 @@ module "primary_region" {
 }
 
 resource "azurerm_key_vault" "main" {
-  #checkov:skip=CKV_AZURE_109: TODO: consider firewall settings, route traffic via VNet
-  #checkov:skip=CKV_AZURE_189: TODO: public network access enabled check
-  #checkov:skip=CKV2_AZURE_32: TODO: private endpoint configuration
-  name                        = format("%s-kv-%s", local.org, local.resource_suffix)
-  location                    = module.primary_region.location
-  resource_group_name         = azurerm_resource_group.primary.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-  enable_rbac_authorization   = true
+  name                          = format("%s-kv-%s", local.org, local.resource_suffix)
+  location                      = module.primary_region.location
+  resource_group_name           = azurerm_resource_group.primary.name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = 7
+  purge_protection_enabled      = true
+  rbac_authorization_enabled    = true
+  public_network_access_enabled = false
+  sku_name                      = "standard"
 
-  sku_name = "standard"
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Deny"
+  }
 
   tags = local.tags
 }
@@ -47,5 +49,24 @@ resource "azurerm_key_vault_secret" "manual_secrets" {
     ignore_changes = [
       value
     ]
+  }
+}
+
+resource "azurerm_private_endpoint" "keyvault" {
+  name                = "${local.org}-pe-keyvault-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "${local.org}-pdns-${local.service_name}-keyvault-${var.environment}"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.keyvault.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.org}-psc-keyvault-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_key_vault.main.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
   }
 }
