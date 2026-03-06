f6ebe24bcdf286bdb0df3a6fc913fa484ad5aa65
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-06 10:53:57 +0000

    feat: creating private endpoint terraform and depends on

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -1,3 +1,12 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${local.org}-vnet-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  address_space       = [var.vnet_config.address_space]
+
+  tags = var.tags
+}
+
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
   name                  = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
   resource_group_name   = var.tooling_config.network_rg
