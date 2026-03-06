59f693c0c52c19b929b695aa4b7bdab9179a6231
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-06 11:21:14 +0000

    feat: creating private endpoint terraform and depends on

--- infrastructure/environments/dev.tfvars
+++ infrastructure/environments/dev.tfvars
@@ -2,6 +2,5 @@ environment = "dev"
 
 vnet_config = {
   address_space             = "10.36.0.0/16"
-  apps_subnet_address_space = "10.36.0.0/24"
   main_subnet_address_space = "10.36.1.0/24"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,7 +13,7 @@ variable "tags" {
 variable "vnet_config" {
   description = "VNet configuration"
   type = object({
-    address_space                       = string
-    main_subnet_address_space           = string
+    address_space             = string
+    main_subnet_address_space = string
   })
 }
