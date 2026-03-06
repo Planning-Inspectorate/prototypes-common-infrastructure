c73b6ebb937bc63987103bfd762af1157464960c
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-06 11:31:37 +0000

    feat: creating private endpoint terraform and depends on

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,13 +1,5 @@
 data "azurerm_client_config" "current" {}
 
-data "azurerm_virtual_network" "tooling" {
-  name                = var.tooling_config.network_name
-  resource_group_name = var.tooling_config.network_rg
-
-  provider = azurerm.tooling
-}
-
-
 data "azurerm_container_registry" "acr" {
   name                = "pinscrsharedtoolinguks"
   resource_group_name = "pins-rg-shared-tooling-uks"
