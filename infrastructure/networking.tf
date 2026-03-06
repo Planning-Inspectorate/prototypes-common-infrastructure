4d035fc007abb90e99b8c588786fdcddf922cbac
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-03-06 11:03:49 +0000

    feat: creating private endpoint terraform and depends on

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -7,6 +7,15 @@ resource "azurerm_virtual_network" "main" {
   tags = var.tags
 }
 
+resource "azurerm_subnet" "main" {
+  #checkov:skip=CKV2_AZURE_31: "Ensure VNET subnet is configured with a Network Security Group (NSG)"
+  name                              = "${local.org}-snet-${local.resource_suffix}"
+  resource_group_name               = azurerm_resource_group.primary.name
+  virtual_network_name              = azurerm_virtual_network.main.name
+  address_prefixes                  = [var.vnet_config.main_subnet_address_space]
+  private_endpoint_network_policies = "Enabled"
+}
+
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
   name                  = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
   resource_group_name   = var.tooling_config.network_rg
