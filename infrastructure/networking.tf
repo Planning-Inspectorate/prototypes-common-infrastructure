9d86986d219d3449a65c4e64d2e1b1e7a73e39d6
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2026-09-11 09:58:40 +0100

    chore(infrastructure): updating TF code for azurerm v5

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -57,10 +57,9 @@ resource "azurerm_virtual_network_peering" "tooling_to_prototypes" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "keyvault" {
-  name                  = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
-  resource_group_name   = var.tooling_config.network_rg
-  private_dns_zone_name = data.azurerm_private_dns_zone.keyvault.name
-  virtual_network_id    = azurerm_virtual_network.main.id
+  name                = "${local.org}-vnetlink-keyvault-${local.resource_suffix}"
+  private_dns_zone_id = data.azurerm_private_dns_zone.keyvault.id
+  virtual_network_id  = azurerm_virtual_network.main.id
 
   tags = local.tags
 
